98b4cc79fb7e573a2c0b5ddf37fd0d58e6f1f5e0
Matthew <matthewrleighton@gmail.com>
2023-02-20 16:10:28 +0100

    Add basic redirect functionality for Fandom site

--- scripts/content.js
+++ scripts/content.js
@@ -11,7 +11,7 @@ function handleWikipedia(selectedDate) {
 
 	// If the user is already on an old revision, don't do anything.
 	if (isAlreadyOldWiki()) {
-		return handleLandOnOldWiki(title);
+		return handleLandOnOldWiki(title, selectedDate);
 	}
 
 	// Checking if the query param specifies that a redirect should be prevented.
@@ -90,10 +90,10 @@ function getQueryParams() {
 }
 
 // Add a message explaining why the user was redirected.
-function handleLandOnOldWiki(title) {
+function handleLandOnOldWiki(title, date) {
 	var queryParams = getQueryParams();
 
-	// If the user arrived here by some other means, don't do anything.
+	// If the user arrived here by some other means (i.e. navigated manually), don't do anything.
 	if (!queryParams.wiki_date_lock_redirected) {
 		return
 	}
@@ -101,17 +101,27 @@ function handleLandOnOldWiki(title) {
 	// Wait for the page to load.
 	window.addEventListener('load', (event) => {
 		const divElement = document.getElementById('mw-content-subtitle');
-		const newElement = document.createElement('p');
-		
-		var backUrl = `https://en.wikipedia.org/w/index.php?title=${title}&wiki_date_lock_prevent_redirect=true`;
-		console.log('backUrl', backUrl);
-
-		var message = `You were redirected here from the <a href="${backUrl}">latest version</a> of this page by the <i>Wiki Date Lock</i> extension.`;
-
-		newElement.innerHTML = "<b style='color:red;'>" + message + "</b>";
 	
-		divElement.prepend(newElement);
+		var backUrl = `https://en.wikipedia.org/w/index.php?title=${title}&wiki_date_lock_prevent_redirect=true`;
+		var humanReadableDate = getHumanReadableDate(date);
+
+		const newElement1 = document.createElement('p');
+		var message1 = `This is the page as it existed on ${humanReadableDate}.`
+		newElement1.innerHTML = "<b style='color:red;'>" + message1 + "</b>";
+
+		const newElement2 = document.createElement('p');
+		var message2 = `You were redirected here from the <a href="${backUrl}">latest version</a> of this page by the <i>Wiki Date Lock</i> extension.`;
+		newElement2.innerHTML = "<b style='color:red;'>" + message2 + "</b>";
+
+	
+		divElement.prepend(newElement1);
+		divElement.prepend(newElement2);
 	});
+}
+
+function getHumanReadableDate(date) {
+	var language = window.navigator.userLanguage || window.navigator.language;
+	return new Date(date).toLocaleDateString(language, { weekday:"long", year:"numeric", month:"long", day:"numeric"})
 }
 
 function handleNonExistentPage(title, date) {
@@ -122,7 +132,9 @@ function handleNonExistentPage(title, date) {
 		const divElement = document.getElementById('bodyContent');
 		const newElement = document.createElement('p');
 		
-		var message = `<b style="color:red;"><i>Wiki Date Lock:</i> The page did not exist on the specified date (${date}). Currently showing live page.</b>`
+		var humanReadableDate = getHumanReadableDate(date);
+
+		var message = `<b style="color:red;"><i>Wiki Date Lock:</i> The page did not exist on the specified date (${humanReadableDate}). Currently showing live page.</b>`
 
 		newElement.innerHTML = message;
 	
@@ -145,8 +157,13 @@ function getPageTitle() {
 
 // Format the date to be in the format that Wikipedia expects.
 // YYYY-MM-DDTHH:MM:SSZ
-function formatSelectedDate(selectedDate) {
-	return selectedDate + 'T00:00:00Z'; 
+function formatSelectedDate(selectedDate, website='wikipedia') {
+	if (website == 'wikipedia') {
+		return selectedDate + 'T00:00:00Z';
+	}
+	
+	// Fandom uses a different format. (YYYYMMDDHHMMSS)
+	return selectedDate.replaceAll('-', '') + '000000';
 }
 
 function redirectToRevision(title, revision_id) {
@@ -160,13 +177,57 @@ function isRedirectPrevented() {
 	return getQueryParams().wiki_date_lock_prevent_redirect;
 }
 
-function handleFandom() {
-	alert("You're on Fandom!");
+function handleFandom(selectedDate) {
+	var title = getPageTitle();
+
+	// If the user is already on an old revision, don't do anything.
+	if (isAlreadyOldWiki()) {
+		// alert('already on old page.')
+		return handleLandOnOldWiki(title, selectedDate);
+	} else {
+		// alert('Not on old page');
+	}
+
+	// On some pages (like the main page), we don't want to redirect.
+	if (isNonRedirectPage(title)) {
+		return;
+	}
+
+	var formattedSelectedDate = formatSelectedDate(selectedDate, 'fandom');
+	var queryUrl = document.location.href + '?offset=' + formattedSelectedDate + '&limit=1&action=history';
+
+	console.log(queryUrl);
+
+	fetch(queryUrl)
+		.then(response => response.text())
+		.then(html => {
+			const parser = new DOMParser();
+			const doc = parser.parseFromString(html, 'text/html');
+			console.log(doc);
+			// const element = doc.querySelector('.mw-tag-visualeditor');
+			const parent = doc.querySelector('#pagehistory');
+			const element = parent.querySelector('li');
+			const revisionId = element.getAttribute('data-mw-revid');
+
+			// alert('redirecting to revision ' + revisionId);
+			window.location.href = document.location.href + '?oldid=' + revisionId;
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+
+
+
 }
 
 function main() {
 	chrome.storage.sync.get('selectedDate', (result) => {
 		var selectedDate = result.selectedDate;
+
+		// If the user hasn't selected a date, don't do anything.
+		if (!selectedDate) {
+			return
+		}
 
 		if (wikipediaRegex.test(window.location.href)) {
 			handleWikipedia(selectedDate);
